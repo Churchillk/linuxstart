158c1fab5dbf7baab2742077ca7429539d93b23f
Churchillk <132873688+Churchillk@users.noreply.github.com>
2023-10-19 08:00:57 +0300

    firts file
    by chu

---
+++ apts.sh
@@ -0,0 +1,14 @@
+sudo su
+apt update
+apt upgrade -y
+apt-get update
+apt-get upgrade -y 
+apt --fix-brocken install
+apt install tor
+apt install remmina
+apt install docker.io -y
+apt install virtualbox
+pip install pynput
+https://github.com/OWASP/QRLJacking.git
+https://github.com/Und3rf10w/kali-anonsurf.git
+https://github.com/htr-tech/zphisher.git
