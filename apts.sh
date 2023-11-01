29938f12d32793328ec14797c994f138c1908f46
Churchillk <132873688+Churchillk@users.noreply.github.com>
2023-11-01 10:09:46 -0400

    more updates

--- apts.sh
+++ apts.sh
@@ -13,5 +13,7 @@ git clone https://github.com/OWASP/QRLJacking.git
 git clone https://github.com/Und3rf10w/kali-anonsurf.git
 git clone https://github.com/htr-tech/zphisher.git
 git clone https://github.com/karma9874/AndroRAT.git
+cd  AndroRAT
+pip install -r requirements.txt
 git clone https://github.com/M4sc3r4n0/Evil-Droid.git
 
